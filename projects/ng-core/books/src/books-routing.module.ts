8f7054486bcb8e651f4da5f8bf7371d3a4b270a6
Alain BOUDARD <aboudard@connectiv-it.com>
2023-11-28 18:37:58 +0100

    fix: books route

--- projects/ng-core/books/src/books-routing.module.ts
+++ projects/ng-core/books/src/books-routing.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { BooksComponent } from './books/books.component';
+import { RouterModule, Routes } from '@angular/router';
 
-
+const routes: Routes = [
+  { path: '', component: BooksComponent }
+];
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
-  ]
+    RouterModule.forChild(routes)
+  ],
+  exports: [RouterModule]
 })
 export class BooksRoutingModule { }
